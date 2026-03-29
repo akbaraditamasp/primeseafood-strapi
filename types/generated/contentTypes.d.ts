1441df871fb12dfdc93aebf3c0ac059bd06e9f79
Akbar Aditama <akbaraditamasp@gmail.com>
2026-03-30 01:42:06 +0700

    fix: update minor version of strapi

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -767,6 +767,13 @@ export interface ApiGeneralGeneral extends Struct.SingleTypeSchema {
     updatedAt: Schema.Attribute.DateTime;
     updatedBy: Schema.Attribute.Relation<'oneToOne', 'admin::user'> &
       Schema.Attribute.Private;
+    whatsapp: Schema.Attribute.String &
+      Schema.Attribute.Required &
+      Schema.Attribute.SetPluginOptions<{
+        i18n: {
+          localized: true;
+        };
+      }>;
   };
 }
 
